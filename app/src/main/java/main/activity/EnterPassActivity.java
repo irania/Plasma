7a1ee97d9c1ac1fdace4c1b2ccf5fef9385f0dd7
atefeh.ir <atefeh.ir72@gmail.com>
2017-09-30 16:06:23 +0330

    show version

--- app/src/main/java/main/activity/EnterPassActivity.java
+++ app/src/main/java/main/activity/EnterPassActivity.java
@@ -2,6 +2,8 @@ package main.activity;
 
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +27,15 @@ public class EnterPassActivity extends MyActivity implements  View.OnClickListen
 
         pass_textview = (TextView) findViewById(R.id.txt_pass);
         setPassText();
+
+        try {
+            PackageInfo pInfo = this.getPackageManager().getPackageInfo(getPackageName(), 0);
+            String version = pInfo.versionName;
+            TextView vt = (TextView) findViewById(R.id.txt_pass_version);
+            vt.setText("version "+version);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
 
     }
